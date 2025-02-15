0d29f4e99e6b8625a8f521cddf99f2f432533cd9
shivakrishnaah <shiva.krishnaah@gmail.com>
2025-02-15 19:04:46 +0530

    Added the flotorch retriever methods such as Guardrails and rerank

--- guardrails/guardrails.py
+++ guardrails/guardrails.py
@@ -18,27 +18,17 @@ class BedrockGuardrail(BaseGuardRail):
         self.guardrail_version = guardrail_version
         self.runtime_client = runtime_client or boto3.client('bedrock-runtime')
         
-    def apply_guardrail(self, content: str,
+    def apply_guardrail(self, text: str,
         source: str = 'INPUT'):
         try:
             request_params = {
                 'guardrailIdentifier': self.guardrail_id,
                 'guardrailVersion': self.guardrail_version,
                 'source': source,
-                'content': content
+                'content': [{"text": text}]
             }
             response = self.runtime_client.apply_guardrail(**request_params)
             return response
-
         except ClientError as e:
             print(f"Error applying guardrail: {str(e)}")
             raise
-        # response = self.runtime_client.apply_guardrail(
-        #     guardrail_id=self.guardrail_id,
-        #     guardrail_version=self.guardrail_version,
-        #     content=[{'text': content}],
-        #     source=source
-        # )
-
-        # if response['action'] == 'GUARDRAIL_INTERVENED':
-        #     assessment
